2f9883194fc0321ec915c8fa23411a58ff3b9bf5
Muana <lalhming.muana1982@gmail.com>
2023-03-21 17:05:31 +0530

    conditional in JSX

--- test/src/App.js
+++ test/src/App.js
@@ -2,10 +2,17 @@ import React, {Fragment, Component} from 'react';
 import './App.css';
 
 function App() {
+  const name ='Joan of ark'
+
+  const loading= false;
+  if(loading){
+    return <h4>Loading ....</h4>
+  }
+
   return (
     <Fragment>
      <h1>Hello from react</h1>
-     <h2>Goodbye</h2>
+     <h2>Test page {name}</h2>
     </Fragment>
   );
 }
